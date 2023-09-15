baaf38ad1674fbb5efeff06305d61b4f06d9cc80
R. Zander <r-zander@users.noreply.github.com>
2023-09-15 11:01:41 +0200

    https://trello.com/c/20WLdOcc/15-default-name-generator
    Started collecting words and name structures

---
+++ js/nameGenerator.js
@@ -0,0 +1,97 @@
+prefixes = ['Astral', 'Celestial', 'Ethereal', 'Mystic', 'Cosmic', 'Arcane', 'Crystal', 'Lunar', 'Dark', 'Chaos', 'Orbital', 'Terror', 'Doom', 'Ancestral', 'Twilight', 'Immortal'];
+core_words = ['Citadel', 'Sanctum', 'Nexus', 'Arcane', 'Tempest', 'Fate', 'Starforge', 'Fortress', 'Moon'];
+suffixes = ['Station', 'Outpost', 'Spire', 'Haven', 'Keep', 'Orb', 'Phalanx', 'Port', 'Pharos', 'Lighthouse'];
+
+
+stationType = ['Citadel', 'Sanctum', 'Nexus', 'Forge', 'Fortress', 'Station', 'Outpost', 'Spire', 'Haven', 'Port' ];
+abstractStation = ['Throne', 'Plate', 'Phalanx', 'Dome'];
+relevantObject = ['Rock', 'Orb', 'Portal'];
+
+// https://dundaxiancodex.fandom.com/wiki/Notable_Locations
+
+
+
+
+mythicalReferences = ['Fenris', 'Elysium', 'Tartarus', 'Empyrean'];
+// dark galactic skies
+// dark wings of steel
+// Home of Laser and Steel
+// Darkstorm Galaxy --> andere Worte können zusammengesetzt werden um "eigenname" zu bilden
+// Galaxy Destruction Force
+// The Last Defense
+// The First Strike
+// Unescapable Prison
+
+// PREFIX?
+// Celestial Forge
+// Astral Storm
+// Warpfire
+// Thunderblade
+// Galactic Prism
+// Necrovoid
+// Aetherian
+// Eldritch
+// Infernoth
+
+// CORE WORDS?
+// Observatory
+// Warforge
+// Warpkeep
+// Arcanium
+// Vortexarium
+// Shadowgate
+
+// SUFFIX?
+// Haven
+// Watch
+// Conflux
+// Bastion
+// Resonance
+// Crucible
+// Nexus
+// Sanctum
+// Pinnacle
+// Asylum
+
+/*
+* Patterns
+*
+* |Station Type|
+*   Examples
+*       Starbase
+*       Phalanx
+*
+* |1 syllable Space Reference||1 syllable Station Type|
+*   Examples
+*       Starbase
+*       Mooncitadel --> nope
+*       Spacesanctum --> nope
+*       Starstation --> maybe
+*       Starforge
+*       Spaceport
+*
+* |Space Reference| |Settlement|
+*   Examples
+*       Cloud City
+*       Space Town
+*       Space Colony
+*
+* |Station Type| |Point of Interest|
+*   Examples
+*       Port Maw
+*       Port Wander
+*       Port Sanctus
+*
+* [Prefix] |Point of Interest| |Station Type|
+*   Examples
+*       Death Star
+*       Cloud City
+*       Blackstone Fortress
+*
+* [Prefix] |Core| |Number|
+*   |Number| = (Zero...One...Twelve) | (roman number) | (A...Z)(-_|)(0 < 2n+1 < 12) | (Latin Ordinal Numbers: Primus, secundus, Tertius, quartus,quintus, sextus, septimus, octavus, nonus,decimus) | (2+)0(Primzahl oder 11n)
+*   Examples
+*       Deep Space Nine
+*       Starbase 1
+*       Space Station K-7
+* */
