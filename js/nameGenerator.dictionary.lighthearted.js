d317d287f536f7062e2a9f51dddd8052e3d69b56
R. Zander <r-zander@users.noreply.github.com>
2023-09-20 12:03:21 +0200

    https://trello.com/c/rzglVhE9/32-name-generator-lighthearted-version

---
+++ js/nameGenerator.dictionary.lighthearted.js
@@ -0,0 +1,309 @@
+const prefixes = [
+    'Algebraic', // Adventure Time
+    'Alphanumeric', // Adventure Time
+    'Astronomical',
+    'Blissful',
+    'Bouncy',
+    'Celestial',
+    'Cheerful',
+    'Comical',
+    'Cosmic',
+    'Crystal',
+    'Dreamy', // Red Dwarf
+    'Eccentric',
+    'Ecstatic',
+    'Enchanted', // Adventure Time
+    'Energetic',
+    'Enthusiastic',
+    'Eternal',
+    'Ethereal',
+    'Euphoric',
+    'Extra',
+    'Fanciful',
+    'Far-Out',
+    'Forever',
+    'Funky', // Adventure Time
+    'Galactic',
+    'Greasy', // Adventure Time
+    'Groovy', // Adventure Time
+    'Hopeful',
+    'Interstellar',
+    'Jolly',
+    'Jubilant',
+    'Lower',
+    'Ludicrous', // Spaceballs
+    'Lunar',
+    'Magical', // Adventure Time
+    'Mathematical', // Adventure Time
+    'Mega',
+    'Mirthful',
+    'Mystic',
+    'New New', // Futurama
+    'New', // Futurama
+    'Optimistic',
+    'Orbital',
+    'Outlandish',
+    'Pantless',
+    'Playful',
+    'Quirky',
+    'Radiant',
+    'Radical', // Adventure Time
+    'Rhombus', // Adventure Time
+    'Snacky', // Adventure Time
+    'Spiral',
+    'Starry',
+    'Sunny',
+    'Super',
+    'Surreal',
+    'Ultra',
+    'Upper',
+    'Wacky', // Adventure Time
+    'Wandering',
+    'Whimsical', // Adventure Time
+    'Whimsy', // Adventure Time
+    'Wholesome',
+    'Yogaballs', // Adventure Time
+    'Zany', // Adventure Time
+];
+const shortAbstractWords = [
+    'Babel', // The Hitchhiker's Guide to the Galaxy
+    'Beach',
+    'Bow', // Adventure Time
+    'Dream', // Adventure Time
+    'Fun', // Adventure Time
+    'Glee',
+    'Hope',
+    // 'Joy', // Adventure Time - TODO seems adult, unless correctly combined
+    'Light',
+    'Play',
+    'Quest', // Galaxy Quest
+    'Quirk',
+    'Rain', // Adventure Time
+    'Schmutz',
+    'Toy',
+];
+let longAbstractWords = [
+    'Adventure', // Adventure Time
+    'Babbling', // The Hitchhiker's Guide to the Galaxy
+    'Biscuit',
+    'Bliss',
+    'Cats',
+    // 'Ecstasy', //  - TODO seems adult, unless correctly combined
+    'Euphoria',
+    'Euphorium',
+    'Express', // Futurama
+    'Fantasy', // Adventure Time
+    'Felicity',
+    'Folly',
+    'Grease',
+    'Harmony',
+    'Imagination', // Adventure Time
+    'Junk',
+    'Laughter', // Adventure Time
+    'Magic',
+    'Melon heart',
+    'Nuts',
+    'Odyssey',
+    'Pants',
+    'Paradise',
+    'Peeps',
+    'Quirks',
+    'Radiance',
+    'Rhombus',
+    'Serenity',
+    'Serenium',
+    'Smiles',
+    'Spiral',
+    'Sunshine',
+    'Twilight',
+    'Unity',
+    'Uplift',
+    'Utopia',
+    'Utopium',
+    'Wonder', // Adventure Time
+    'Yogaballs',
+];
+
+const shortSpaceReferences = [
+    'Cloud',
+    'Jet',
+    'Moon',
+    'Nova',
+    'Sky',
+    'Space',
+    'Star',
+    'Sun',
+];
+const longSpaceReferences = [
+    'Asteroid',
+    'Exoplanet',
+    'Galaxy',
+    'Jetstream',
+    'Nebula',
+    'Orbit',
+    'Outer Space',
+    'Planet',
+    'Planetoid',
+    'Quasar',
+    'Supernova',
+    'Universe',
+];
+
+const places = [
+    'Bastion',
+    'Burg',
+    'Castle',
+    'Citadel',
+    'City',
+    'Cove',
+    'Dungeon',
+    'Forest', // Adventure Time
+    'Haven',
+    'Hills',
+    'Home',
+    'House', // Adventure Time
+    'Kingdom', // Adventure Time
+    'Knowhere', // Guardians of the Galaxy
+    'Land', // Adventure Time
+    'Oasis',
+    'Outpost',
+    'Palace',
+    'Port',
+    'Restaurant', // The Hitchhiker's Guide to the Galaxy
+    'Ridge',
+    'Sanctum',
+    'Springs',
+    'Town',
+    'Tree', // Adventure Time
+    'Vaultopolis',
+    'Ville',
+    'Watch',
+    'York', // Futurama
+];
+const shortStationTypes = [
+    'Base',
+    'Core',
+    'Gate',
+    'Hub',
+    'Icon',
+    'Keep',
+    'Plex',
+    'Port',
+    'Ship',
+    'Tex',
+    'Topia',
+    'Tron',
+];
+const longStationTypes = [
+    'Citadel',
+    'Engine',
+    'Explorer',
+    'Haven',
+    'Lighthouse',
+    'Navigator',
+    'Observatory',
+    'Outpost',
+    'Pinnacle',
+    'Station',
+    'UFO', // actually short, but an abbreviation
+];
+/**
+ * Actually space references that could also indicate a giant space station.
+ */
+const oversizedStationTypes = [
+    'Antiplanet',
+    'Asteroid',
+    'Earth',
+    'Moon',
+    'Orb',
+    'Portal',
+    'Protoplanet',
+    'Rock',
+    'Star',
+    'World',
+];
+
+const abstractStations = [
+    'Ball', // Spaceballs
+    'Bug', // Red Dwarf
+    'Corps', // Guardians of the Galaxy
+    'Donut', // Futurama
+    'Dreamer',
+    'Eden',
+    'Funhouse', // Adventure Time
+    'Guardian',
+    'Playground', // Adventure Time
+    'Point',
+    'Prism',
+    'Protector', // Galaxy Quest
+    'Throne',
+    'Vault',
+    'Wanderer',
+];
+const colorWords = [
+    'Bliss',
+    'Blue',
+    'Joy',
+    'Lemon', // Adventure Time
+    'Neon',
+    'Pink',
+    'Rose',
+];
+
+const elementWords = [
+    'Candy', // Adventure Time
+    'Dust',
+    'Fire',
+    'Ice', // Adventure Time
+    'Laser',
+    'Light',
+    'Plasma',
+    'Spark',
+    'Stream',
+];
+
+// TODO unused words, could be integrated later on
+const moreWords = [
+    'End of the Universe', // The Hitchhiker's Guide to the Galaxy
+    '42', // The Hitchhiker's Guide to the Galaxy
+]
+// end of unused words
+
+const numberWords = [
+    'One',
+    'Two',
+    'Three',
+    'Seven',
+    'Eleven',
+];
+
+const unevenNumbers = [2, 3, 7, 13].map(number => String(number));
+const thousands = [3, 5, 7, 9].map(number => String(1000 * number));
+
+/**
+ * Prime numbers and 11*n
+ */
+const interestingNumbers = [
+    11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    22, 33, 44, 55, 66, 77, 88, 99
+];
+
+const yearNumbers = interestingNumbers
+    .map(number => _.random(2, 9) * 1000 + number)
+    .map(number => String(number));
+
+const romanNumbers = [
+    1, 2, 3, 5, 10,
+].map(convertToRoman);
+
+const numbering = () => [
+    new NameGenerator('Any Number Display')
+        .add([
+            numberWords,
+            romanNumbers,
+            unevenNumbers,
+            thousands,
+            yearNumbers,
+        ])
+        .bound(),
+];
--- js/nameGenerator.js
+++ js/nameGenerator.js
@@ -4,236 +4,12 @@ function mergeWordLists(...lists) {
     return _.union(_.flatten(lists));
 }
 
-const prefixes = [
-    'Ancestral',
-    'Arcane',
-    'Ascended',
-    'Astral',
-    'Celestial',
-    'Chosen',
-    'Cosmic',
-    'Crystal',
-    'Dark',
-    'Doomed',
-    'Eldritch',
-    'Eternal',
-    'Ethereal',
-    'First',
-    'Galactic',
-    'Haunted',
-    'Immortal',
-    'Impenetrable',
-    'Interplanetary',
-    'Last',
-    'Lunar',
-    'Mystic',
-    'Orbital',
-    'Unescapable',
-];
-const shortAbstractWords = [
-    'Doom',
-    'Fate',
-    'Light',
-    'Null',
-    'Shadow',
-    'Storm',
-    'Void',
-    'War',
-];
-const abstractWords = mergeWordLists(shortAbstractWords, [
-    'Chaos',
-    'Death',
-    'Destruction',
-    'Force',
-    'Immortality',
-    'Inferno',
-    'Tempest',
-    'Terror',
-    'Twilight',
-    'Vengeance',
-]);
-
-const shortSpaceReferences = [
-    'Cloud',
-    'Moon',
-    'Sky',
-    'Space',
-    'Star',
-    'Thunder',
-    'Warp',
-];
-const longSpaceReferences = [
-    'Asteroid',
-    'Deep Space',
-    'Galaxy',
-    'Nebula',
-    'Orbit',
-    'Outer Space',
-    'Planet',
-    'Supernova',
-];
-
+const abstractWords = mergeWordLists(shortAbstractWords, longAbstractWords);
 const spaceReferences = mergeWordLists(shortSpaceReferences, longSpaceReferences);
-
-const places = [
-    'Bastion',
-    'Castle',
-    'Citadel',
-    'City',
-    'Colony',
-    'Fortress',
-    'Haven',
-    'Home',
-    'Megalopolis',
-    'Metropolis',
-    'Outpost',
-    'Palace',
-    'Port',
-    'Sanctum',
-    'Stronghold',
-    'Town',
-    'Watch',
-];
-const shortStationTypes = [
-    'Base',
-    'Forge',
-    'Gate',
-    'Keep',
-    'Port',
-    'Spire',
-];
-const longStationTypes = [
-    'Asylum',
-    'Citadel',
-    'Commando Base',
-    'Crucible',
-    'Fortress',
-    'Haven',
-    'Lighthouse',
-    'Nexus',
-    'Observatory',
-    'Outpost',
-    'Pharos',
-    'Pinnacle',
-    'Sanctum',
-    'Spire',
-    'Station',
-];
-/**
- * Actually space references that could also indicate a giant space station.
- */
-const oversizedStationTypes = [
-    'Asteroid',
-    'Earth',
-    'Moon',
-    'Orb',
-    'Portal',
-    'Rock',
-    'Star',
-];
-
-const abstractStations = [
-    'Conflux',
-    'Defense',
-    'Destroyer',
-    'Dome',
-    'Machine',
-    'Phalanx',
-    'Plate',
-    'Prism',
-    'Prison',
-    'Throne',
-    'War Machine',
-];
 const stationTypes = mergeWordLists(shortStationTypes, longStationTypes);
-const colorWords = [
-    'Black',
-    'Blue',
-    'Dark',
-    'Neon',
-    'Red',
-];
 
-const elementWords = [
-    'Fire',
-    'Laser',
-    'Light',
-    'Steel',
-    'Stone',
-    'Storm',
-    'Stream',
-];
 
-// TODO unused words, could be integrated later on
-// const uncategorized = [
-//     'neutreon power',
-// ];
-// const longColor = [
-//     'Purple',
-//     'Violett',
-// ];
-// const mythicalReferences = [
-//     'Fenris',
-//     'Elysium',
-//     'Tartarus',
-//     'Empyrean'
-// ];
-// const moreSuffices = [
-//     'Reborn',
-//     'United',
-//     'in the sky',
-//     'of Chaos',
-// ];
-// end of unused words
 
-const numberWords = [
-    'Zero',
-    'One',
-    'Two',
-    'Three',
-    'Four',
-    'Five',
-    'Six',
-    'Seven',
-    'Eight',
-    'Nine',
-    'Ten',
-    'Eleven',
-    'Twelve'
-];
-
-const unevenNumbers = [1, 3, 5, 7, 9, 11, 13].map(number => String(number));
-const thousands = [3, 5, 7, 9].map(number => String(1000 * number));
-
-/**
- * Prime numbers and 11*n
- */
-const interestingNumbers = [
-    11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
-    22, 33, 44, 55, 66, 77, 88, 99
-];
-
-const yearNumbers = interestingNumbers
-    .map(number => _.random(2, 9) * 1000 + number)
-    .map(number => String(number));
-
-const latinOrdinalNumbers = [
-    'Primus',
-    'Secundus',
-    'Tertius',
-    'Quartus',
-    'Quintus',
-    'Sextus',
-    'Septimus',
-    'Octavus',
-    'Nonus',
-    'Decimus'
-];
-
-const romanNumbers = [ // Excluded numbers are too long/unaesthetic
-    1, 2, 3, 4, 5, 6, 7,/* 8, */ 9, 10,
-    11, 12/*,13*/, 14, 15, 16/*,17,18*/, 19
-].map(convertToRoman);
 
 class NameGenerator {
     #name = '';
@@ -330,7 +106,7 @@ class NameGenerator {
         // Strings are just used - they are the exit condition for the recursive loop
         if (typeof namePart === 'string') {
             if (this.#lowercasePartIndex.includes(index)) {
-                return namePart.substr(0, 1).toLowerCase() + namePart.substr(1);
+                return _.lowerFirst(namePart);
             }
             return namePart;
         }
@@ -358,37 +134,7 @@ class NameGenerator {
     }
 }
 
-const hairSpace = '\u200A';
 
-const numbering = [
-    new NameGenerator('Any Number Display')
-        .add([
-            numberWords,
-            romanNumbers,
-            unevenNumbers,
-            thousands,
-            yearNumbers,
-            latinOrdinalNumbers
-        ])
-        .bound(),
-    new NameGenerator('Letter Separator Number')
-        .add('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))
-        .skipSeparator()
-        .add(['-', '_', hairSpace + '|' + hairSpace, '/', ':'])
-        .skipSeparator()
-        .add([
-            unevenNumbers,
-            thousands,
-        ])
-        .bound(),
-    new NameGenerator('Letter Separator Roman number')
-        .add('ABCDEGHJKLMNOPQRSTUW'.split(''))
-        .skipSeparator()
-        .add(['-', hairSpace + '|' + hairSpace, ':'])
-        .skipSeparator()
-        .add(romanNumbers)
-        .bound()
-];
 const stationNameGenerator = new NameGenerator()
     .add([
         /**
